b84ed801bc63f265e8a0cc799cd4070365d6f38e
Giovane Souza <developergiovanesouza@gmail.com>
2023-12-30 20:57:58 -0300

    feat: add propriedades e ajustes no construtor

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,16 +1,25 @@
 namespace DesafioPOO.Models
 {
+    // Classe abstrata -> não pode ser instanciada
     public abstract class Smartphone
     {
+        // Atributos/Propriedades
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
 
-        public Smartphone(string numero)
+
+        // Construtor
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
+        // Métodos
         public void Ligar()
         {
             Console.WriteLine("Ligando...");
